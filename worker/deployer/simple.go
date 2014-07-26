b78a272387fc10e51cb1b3624bd0c09d966d2845
Gabriel Adrian Samfira <gsamfira@cloudbasesolutions.com>
2014-07-26 17:41:04 +0300

    fixed deployer

--- worker/deployer/simple.go
+++ worker/deployer/simple.go
@@ -15,6 +15,7 @@ import (
 
 	"github.com/juju/juju/agent"
 	"github.com/juju/juju/agent/tools"
+	jujunames "github.com/juju/juju/juju/names"
 	"github.com/juju/juju/juju/osenv"
 	"github.com/juju/juju/service"
 	"github.com/juju/juju/service/common"
@@ -134,7 +135,7 @@ func (ctx *SimpleContext) DeployUnit(unitName, initialPassword string) (err erro
 
 	// Install an upstart job that runs the unit agent.
 	cmd := strings.Join([]string{
-		path.Join(toolsDir, "jujud"), "unit",
+		path.Join(toolsDir, jujunames.Jujud), "unit",
 		"--data-dir", dataDir,
 		"--unit-name", unitName,
 		"--debug", // TODO: propagate debug state sensibly
