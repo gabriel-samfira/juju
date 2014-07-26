20bae9cf1f3b6c09804b83f77b819dc14714bf31
Gabriel Adrian Samfira <gsamfira@cloudbasesolutions.com>
2014-07-26 15:29:49 +0300

    Added windows service support

---
+++ cmd/jujud/main_windows.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"os"
+	"syscall"
+
+	"bitbucket.org/kardianos/service"
+)
+
+func runService() {
+	var name = "juju"
+	var displayName = "juju service"
+	var desc = "juju service"
+
+	var s, err = service.NewService(name, displayName, desc)
+	if err != nil {
+		fmt.Errorf("%s", err)
+	}
+
+	err = s.Run(func() error {
+		// start
+		go Main(os.Args)
+		return nil
+	}, func() error {
+		// stop
+		os.Exit(0)
+		return nil
+	})
+
+	if err != nil {
+		s.Error(err.Error())
+	}
+}
+
+func runConsole() {
+	Main(os.Args)
+}
+
+func main() {
+	var mode uint32
+	err := syscall.GetConsoleMode(syscall.Stdin, &mode)
+
+	isConsole := err == nil
+
+	if isConsole == true {
+		runConsole()
+	} else {
+		runService()
+	}
+}
