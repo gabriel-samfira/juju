86f9bf62dea152a691f8fc632d2eb76a0c0c6d63
Gabriel Adrian Samfira <gsamfira@cloudbasesolutions.com>
2014-09-02 05:51:30 +0300

    Added reboot apiserver

---
+++ apiserver/reboot/reboot.go
@@ -0,0 +1,106 @@
+package reboot
+
+import (
+	"github.com/juju/errors"
+	"github.com/juju/loggo"
+	"github.com/juju/names"
+
+	"github.com/juju/juju/apiserver/common"
+	"github.com/juju/juju/apiserver/params"
+	"github.com/juju/juju/state"
+	"github.com/juju/juju/state/watcher"
+)
+
+var logger = loggo.GetLogger("juju.apiserver.reboot")
+
+// RebootAPI provides access to the Upgrader API facade.
+type RebootAPI struct {
+	auth      common.Authorizer
+	st        *state.State
+	machine   *state.Machine
+	resources *common.Resources
+}
+
+func init() {
+	common.RegisterStandardFacade("Reboot", 0, NewRebootAPI)
+}
+
+// NewRebootAPI creates a new client-side RebootAPI facade.
+func NewRebootAPI(
+	st *state.State,
+	resources *common.Resources,
+	auth common.Authorizer,
+) (*RebootAPI, error) {
+	if !auth.AuthMachineAgent() {
+		return nil, common.ErrPerm
+	}
+
+	tag := auth.GetAuthTag().(names.MachineTag)
+	machine, err := st.Machine(tag.Id())
+	if err != nil {
+		return nil, err
+	}
+	return &RebootAPI{
+		st:        st,
+		machine:   machine,
+		resources: resources,
+		auth:      auth,
+	}, nil
+}
+
+// WatchForRebootEvent starts a watcher to track if there is a new
+// reboot request on the machines ID or any of its parents (in case we are a container).
+func (r *RebootAPI) WatchForRebootEvent() (params.NotifyWatchResult, error) {
+	err := common.ErrPerm
+
+	var result params.NotifyWatchResult
+
+	if r.auth.AuthOwner(r.machine.Tag()) {
+		err = nil
+		watch, err := r.machine.WatchForRebootEvent()
+		if err != nil {
+			return params.NotifyWatchResult{}, errors.Trace(err)
+		}
+		// Consume the initial event. Technically, API
+		// calls to Watch 'transmit' the initial event
+		// in the Watch response. But NotifyWatchers
+		// have no state to transmit.
+		if _, ok := <-watch.Changes(); ok {
+			result.NotifyWatcherId = r.resources.Register(watch)
+			err = nil
+		} else {
+			err = watcher.MustErr(watch)
+		}
+	}
+	result.Error = common.ServerError(err)
+	return result, nil
+}
+
+// RequestReebot sets the reboot flag to true for the current machine.
+func (r *RebootAPI) RequestReboot() (params.ErrorResult, error) {
+	logger.Infof("Got reboot request from: %v", r.machine.Tag())
+	err := r.machine.SetRebootFlag(true)
+	if err != nil {
+		return params.ErrorResult{}, errors.Trace(err)
+	}
+	return params.ErrorResult{Error: common.ServerError(err)}, nil
+}
+
+// ClearReboot clears the reboot flag for the current machine.
+func (r *RebootAPI) ClearReboot() (params.ErrorResult, error) {
+	logger.Infof("Got clear reboot request from: %v", r.machine.Tag())
+	err := r.machine.SetRebootFlag(false)
+	if err != nil {
+		return params.ErrorResult{}, errors.Trace(err)
+	}
+	return params.ErrorResult{Error: common.ServerError(err)}, nil
+}
+
+// GetRebootAction gets the reboot flag for the current machine.
+func (r *RebootAPI) GetRebootAction() (params.RebootActionResult, error) {
+	rAction, err := r.machine.ShouldRebootOrShutdown()
+	if err != nil {
+		return params.RebootActionResult{RebootAction: params.ShouldDoNothing}, errors.Trace(err)
+	}
+	return params.RebootActionResult{RebootAction: rAction}, nil
+}
---
+++ apiserver/reboot/reboot_test.go
@@ -0,0 +1,320 @@
+// Copyright 2014 Cloudbase Solutions SRL
+// Copyright 2014 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package reboot_test
+
+import (
+	gc "launchpad.net/gocheck"
+
+	"github.com/juju/juju/apiserver/common"
+	"github.com/juju/juju/apiserver/params"
+	"github.com/juju/juju/apiserver/reboot"
+	apiservertesting "github.com/juju/juju/apiserver/testing"
+	"github.com/juju/juju/instance"
+	jujutesting "github.com/juju/juju/juju/testing"
+	"github.com/juju/juju/state"
+	statetesting "github.com/juju/juju/state/testing"
+)
+
+type machines struct {
+	machine    *state.Machine
+	authorizer apiservertesting.FakeAuthorizer
+	resources  *common.Resources
+
+	rebootAPI *reboot.RebootAPI
+}
+
+type rebootSuite struct {
+	jujutesting.JujuConnSuite
+
+	machine         *machines
+	container       *machines
+	nestedContainer *machines
+}
+
+var _ = gc.Suite(&rebootSuite{})
+
+func (s *rebootSuite) setUpMachine(c *gc.C, machine *state.Machine) *machines {
+	// Create a FakeAuthorizer so we can check permissions,
+	// set up assuming we logged in as a machine agent.
+	authorizer := apiservertesting.FakeAuthorizer{
+		Tag: machine.Tag(),
+	}
+
+	resources := common.NewResources()
+
+	rebootAPI, err := reboot.NewRebootAPI(s.State, resources, authorizer)
+	c.Assert(err, gc.IsNil)
+
+	return &machines{machine, authorizer, resources, rebootAPI}
+}
+
+func (s *rebootSuite) SetUpTest(c *gc.C) {
+	s.JujuConnSuite.SetUpTest(c)
+	var err error
+
+	template := state.MachineTemplate{
+		Series: "quantal",
+		Jobs:   []state.MachineJob{state.JobHostUnits},
+	}
+
+	machine, err := s.State.AddMachine("quantal", state.JobHostUnits)
+	c.Assert(err, gc.IsNil)
+
+	container, err := s.State.AddMachineInsideMachine(template, machine.Id(), instance.LXC)
+	c.Assert(err, gc.IsNil)
+
+	nestedContainer, err := s.State.AddMachineInsideMachine(template, container.Id(), instance.KVM)
+	c.Assert(err, gc.IsNil)
+
+	s.machine = s.setUpMachine(c, machine)
+	s.container = s.setUpMachine(c, container)
+	s.nestedContainer = s.setUpMachine(c, nestedContainer)
+}
+
+func (s *rebootSuite) TearDownTest(c *gc.C) {
+	if s.machine.resources != nil {
+		s.machine.resources.StopAll()
+	}
+
+	if s.container.resources != nil {
+		s.container.resources.StopAll()
+	}
+
+	if s.nestedContainer.resources != nil {
+		s.nestedContainer.resources.StopAll()
+	}
+	s.JujuConnSuite.TearDownTest(c)
+}
+
+func (s *rebootSuite) TestWatchForRebootEvent(c *gc.C) {
+	result, err := s.machine.rebootAPI.WatchForRebootEvent()
+	c.Assert(err, gc.IsNil)
+	c.Check(result.NotifyWatcherId, gc.Not(gc.Equals), "")
+	c.Check(result.Error, gc.IsNil)
+
+	resource := s.machine.resources.Get(result.NotifyWatcherId)
+	c.Check(resource, gc.NotNil)
+
+	w := resource.(state.NotifyWatcher)
+	wc := statetesting.NewNotifyWatcherC(c, s.State, w)
+	wc.AssertNoChange()
+
+	err = s.machine.machine.SetRebootFlag(true)
+	c.Assert(err, gc.IsNil)
+
+	wc.AssertOneChange()
+
+	statetesting.AssertStop(c, w)
+	wc.AssertClosed()
+}
+
+func (s *rebootSuite) TestRequestReboot(c *gc.C) {
+	result, err := s.machine.rebootAPI.WatchForRebootEvent()
+	c.Assert(err, gc.IsNil)
+	c.Check(result.NotifyWatcherId, gc.Not(gc.Equals), "")
+	c.Check(result.Error, gc.IsNil)
+
+	resource := s.machine.resources.Get(result.NotifyWatcherId)
+	c.Check(resource, gc.NotNil)
+
+	w := resource.(state.NotifyWatcher)
+	wc := statetesting.NewNotifyWatcherC(c, s.State, w)
+	wc.AssertNoChange()
+
+	errResult, err := s.machine.rebootAPI.RequestReboot()
+	c.Assert(err, gc.IsNil)
+	c.Assert(errResult.Error, gc.IsNil)
+
+	wc.AssertOneChange()
+
+	res, err := s.machine.rebootAPI.GetRebootAction()
+	c.Assert(err, gc.IsNil)
+	c.Assert(res.RebootAction, gc.Equals, params.ShouldReboot)
+
+	statetesting.AssertStop(c, w)
+	wc.AssertClosed()
+}
+
+func (s *rebootSuite) TestClearReboot(c *gc.C) {
+	result, err := s.machine.rebootAPI.WatchForRebootEvent()
+	c.Assert(err, gc.IsNil)
+	c.Check(result.NotifyWatcherId, gc.Not(gc.Equals), "")
+	c.Check(result.Error, gc.IsNil)
+
+	resource := s.machine.resources.Get(result.NotifyWatcherId)
+	c.Check(resource, gc.NotNil)
+
+	w := resource.(state.NotifyWatcher)
+	wc := statetesting.NewNotifyWatcherC(c, s.State, w)
+	wc.AssertNoChange()
+
+	errResult, err := s.machine.rebootAPI.RequestReboot()
+	c.Assert(err, gc.IsNil)
+	c.Assert(errResult.Error, gc.IsNil)
+
+	wc.AssertOneChange()
+
+	res, err := s.machine.rebootAPI.GetRebootAction()
+	c.Assert(err, gc.IsNil)
+	c.Assert(res.RebootAction, gc.Equals, params.ShouldReboot)
+
+	errResult, err = s.machine.rebootAPI.ClearReboot()
+	c.Assert(err, gc.IsNil)
+	c.Assert(errResult.Error, gc.IsNil)
+
+	res, err = s.machine.rebootAPI.GetRebootAction()
+	c.Assert(err, gc.IsNil)
+	c.Assert(res.RebootAction, gc.Equals, params.ShouldDoNothing)
+
+	statetesting.AssertStop(c, w)
+	wc.AssertClosed()
+}
+
+func (s *rebootSuite) TestWatchForRebootEventFromContainer(c *gc.C) {
+	// Watcher for the machine
+	resultMachine, err := s.machine.rebootAPI.WatchForRebootEvent()
+	c.Assert(err, gc.IsNil)
+	c.Check(resultMachine.NotifyWatcherId, gc.Not(gc.Equals), "")
+	c.Check(resultMachine.Error, gc.IsNil)
+
+	resourceMachine := s.machine.resources.Get(resultMachine.NotifyWatcherId)
+	c.Check(resourceMachine, gc.NotNil)
+
+	w := resourceMachine.(state.NotifyWatcher)
+	wc := statetesting.NewNotifyWatcherC(c, s.State, w)
+	wc.AssertNoChange()
+
+	// Watcher for the container
+	resultContainer, err := s.container.rebootAPI.WatchForRebootEvent()
+	c.Assert(err, gc.IsNil)
+	c.Check(resultContainer.NotifyWatcherId, gc.Not(gc.Equals), "")
+	c.Check(resultContainer.Error, gc.IsNil)
+
+	resourceContainer := s.container.resources.Get(resultContainer.NotifyWatcherId)
+	c.Check(resourceContainer, gc.NotNil)
+
+	wContainer := resourceContainer.(state.NotifyWatcher)
+	wcContainer := statetesting.NewNotifyWatcherC(c, s.State, wContainer)
+	wcContainer.AssertNoChange()
+
+	// Watcher for the nestedContainer
+	resultNestedContainer, err := s.nestedContainer.rebootAPI.WatchForRebootEvent()
+	c.Assert(err, gc.IsNil)
+	c.Check(resultNestedContainer.NotifyWatcherId, gc.Not(gc.Equals), "")
+	c.Check(resultNestedContainer.Error, gc.IsNil)
+
+	resourceNestedContainer := s.nestedContainer.resources.Get(resultNestedContainer.NotifyWatcherId)
+	c.Check(resourceNestedContainer, gc.NotNil)
+
+	wNestedContainer := resourceNestedContainer.(state.NotifyWatcher)
+	wcNestedContainer := statetesting.NewNotifyWatcherC(c, s.State, wNestedContainer)
+	wcNestedContainer.AssertNoChange()
+
+	// Request reboot on the root machine: all machines should see it
+	// machine should reboot
+	// container should shutdown
+	// nested container should shutdown
+	errResult, err := s.machine.rebootAPI.RequestReboot()
+	c.Assert(err, gc.IsNil)
+	c.Assert(errResult.Error, gc.IsNil)
+
+	wc.AssertOneChange()
+	wcContainer.AssertOneChange()
+	wcNestedContainer.AssertOneChange()
+
+	res, err := s.machine.rebootAPI.GetRebootAction()
+	c.Assert(err, gc.IsNil)
+	c.Assert(res.RebootAction, gc.Equals, params.ShouldReboot)
+
+	res, err = s.container.rebootAPI.GetRebootAction()
+	c.Assert(err, gc.IsNil)
+	c.Assert(res.RebootAction, gc.Equals, params.ShouldShutdown)
+
+	res, err = s.nestedContainer.rebootAPI.GetRebootAction()
+	c.Assert(err, gc.IsNil)
+	c.Assert(res.RebootAction, gc.Equals, params.ShouldShutdown)
+
+	errResult, err = s.machine.rebootAPI.ClearReboot()
+	c.Assert(err, gc.IsNil)
+	c.Assert(errResult.Error, gc.IsNil)
+
+	wc.AssertOneChange()
+	wcContainer.AssertOneChange()
+	wcNestedContainer.AssertOneChange()
+
+	// Request reboot on the container: container and nested container should see it
+	// machine should do nothing
+	// container should reboot
+	// nested container should shutdown
+	errResult, err = s.container.rebootAPI.RequestReboot()
+	c.Assert(err, gc.IsNil)
+	c.Assert(errResult.Error, gc.IsNil)
+
+	wc.AssertNoChange()
+	wcContainer.AssertOneChange()
+	wcNestedContainer.AssertOneChange()
+
+	res, err = s.machine.rebootAPI.GetRebootAction()
+	c.Assert(err, gc.IsNil)
+	c.Assert(res.RebootAction, gc.Equals, params.ShouldDoNothing)
+
+	res, err = s.container.rebootAPI.GetRebootAction()
+	c.Assert(err, gc.IsNil)
+	c.Assert(res.RebootAction, gc.Equals, params.ShouldReboot)
+
+	res, err = s.nestedContainer.rebootAPI.GetRebootAction()
+	c.Assert(err, gc.IsNil)
+	c.Assert(res.RebootAction, gc.Equals, params.ShouldShutdown)
+
+	errResult, err = s.container.rebootAPI.ClearReboot()
+	c.Assert(err, gc.IsNil)
+	c.Assert(errResult.Error, gc.IsNil)
+
+	wc.AssertNoChange()
+	wcContainer.AssertOneChange()
+	wcNestedContainer.AssertOneChange()
+
+	// Request reboot on the container: container and nested container should see it
+	// machine should do nothing
+	// container should do nothing
+	// nested container should reboot
+	errResult, err = s.nestedContainer.rebootAPI.RequestReboot()
+	c.Assert(err, gc.IsNil)
+	c.Assert(errResult.Error, gc.IsNil)
+
+	wc.AssertNoChange()
+	wcContainer.AssertNoChange()
+	wcNestedContainer.AssertOneChange()
+
+	res, err = s.machine.rebootAPI.GetRebootAction()
+	c.Assert(err, gc.IsNil)
+	c.Assert(res.RebootAction, gc.Equals, params.ShouldDoNothing)
+
+	res, err = s.container.rebootAPI.GetRebootAction()
+	c.Assert(err, gc.IsNil)
+	c.Assert(res.RebootAction, gc.Equals, params.ShouldDoNothing)
+
+	res, err = s.nestedContainer.rebootAPI.GetRebootAction()
+	c.Assert(err, gc.IsNil)
+	c.Assert(res.RebootAction, gc.Equals, params.ShouldReboot)
+
+	errResult, err = s.nestedContainer.rebootAPI.ClearReboot()
+	c.Assert(err, gc.IsNil)
+	c.Assert(errResult.Error, gc.IsNil)
+
+	wc.AssertNoChange()
+	wcContainer.AssertNoChange()
+	wcNestedContainer.AssertOneChange()
+
+	// Stop watchers
+	statetesting.AssertStop(c, w)
+	wc.AssertClosed()
+
+	statetesting.AssertStop(c, wContainer)
+	wcContainer.AssertClosed()
+
+	statetesting.AssertStop(c, wNestedContainer)
+	wcNestedContainer.AssertClosed()
+}
