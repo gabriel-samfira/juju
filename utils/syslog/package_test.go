c752bc12c0981851d652d46f29721fe574862b8b
bogdanteleaga <bteleaga@cloudbasesolutions.com>
2015-02-01 21:43:26 +0200

    Skipped utils/syslog tests on windows

--- utils/syslog/package_test.go
+++ utils/syslog/package_test.go
@@ -4,11 +4,15 @@
 package syslog_test
 
 import (
+	"runtime"
 	"testing"
 
 	gc "gopkg.in/check.v1"
 )
 
 func TestPackage(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		t.Skip("Skipping rsyslog tests on windows")
+	}
 	gc.TestingT(t)
 }
